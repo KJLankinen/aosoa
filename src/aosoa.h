4663eb294f37ac446e7905f77fa67dc4189607d1
Juhana Lankinen <juhana.lankinen@csc.fi>
2024-03-25 15:34:23 +0200

    Refactor to a different file

--- src/aosoa.h
+++ src/aosoa.h
@@ -43,51 +43,6 @@
 // - add cuda/hip/sycl memory ops
 
 namespace detail {
-// Find the index of a string
-template <CompileTimeString MatchStr> struct Find {
-    // ... from a pack of strings
-    template <CompileTimeString... Candidates> struct FromStrings {
-      private:
-        template <size_t N> consteval static size_t find() { return ~0ul; }
-
-        template <size_t N, CompileTimeString Head, CompileTimeString... Tail>
-        consteval static size_t find() {
-            if constexpr (MatchStr == Head) {
-                return N;
-            } else {
-                return find<N + 1, Tail...>();
-            }
-        }
-
-      public:
-        constexpr static size_t index = find<0, Candidates...>();
-    };
-
-    // ... from a pack of Variables
-    template <typename... Candidates> struct FromVariables {
-        constexpr static size_t index =
-            FromStrings<VariableTraits<Candidates>::name...>::index;
-    };
-};
-
-// - Check if the string is in the parameter pack
-template <CompileTimeString MatchStr> struct Is {
-    template <CompileTimeString... Strings> struct ContainedIn {
-        constexpr static bool value =
-            Find<MatchStr>::template FromStrings<Strings...>::index !=
-            Find<MatchStr>::template FromStrings<>::index;
-    };
-};
-
-// ==== GetType ====
-// - Get index and type corresponding to Cts
-template <CompileTimeString Cts, typename... Variables> struct GetType {
-    static constexpr size_t i =
-        Find<Cts>::template FromVariables<Variables...>::index;
-    using Type =
-        typename NthType<i, typename VariableTraits<Variables>::Type...>::Type;
-};
-
 // ==== MemoryOperations ====
 // - These are used by StructureOfArrays to perform
 //     - memory allocation at construction
--- src/type_operations.h
+++ src/type_operations.h
@@ -18,6 +18,8 @@
 
 #pragma once
 
+#include "compile_time_string.h"
+#include "variable.h"
 #include <cstddef>
 #include <type_traits>
 
@@ -46,4 +48,49 @@ template <size_t N, typename... Types> struct NthType {
   public:
     using Type = std::invoke_result_t<decltype(ofType<0, Types...>)>;
 };
+
+// Find the index of a string
+template <CompileTimeString MatchStr> struct Find {
+    // ... from a pack of strings
+    template <CompileTimeString... Candidates> struct FromStrings {
+      private:
+        template <size_t N> consteval static size_t find() { return ~0ul; }
+
+        template <size_t N, CompileTimeString Head, CompileTimeString... Tail>
+        consteval static size_t find() {
+            if constexpr (MatchStr == Head) {
+                return N;
+            } else {
+                return find<N + 1, Tail...>();
+            }
+        }
+
+      public:
+        constexpr static size_t index = find<0, Candidates...>();
+    };
+
+    // ... from a pack of Variables
+    template <typename... Candidates> struct FromVariables {
+        constexpr static size_t index =
+            FromStrings<VariableTraits<Candidates>::name...>::index;
+    };
+};
+
+// - Check if the string is in the parameter pack
+template <CompileTimeString MatchStr> struct Is {
+    template <CompileTimeString... Strings> struct ContainedIn {
+        constexpr static bool value =
+            Find<MatchStr>::template FromStrings<Strings...>::index !=
+            Find<MatchStr>::template FromStrings<>::index;
+    };
+};
+
+// - Get index and type corresponding to Cts
+template <CompileTimeString Cts, typename... Variables> struct GetType {
+    static constexpr size_t i =
+        Find<Cts>::template FromVariables<Variables...>::index;
+    using Type =
+        typename NthType<i, typename VariableTraits<Variables>::Type...>::Type;
+};
+
 } // namespace detail
