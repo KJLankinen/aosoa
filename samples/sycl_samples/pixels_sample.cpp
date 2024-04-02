56c64349eb81f07560badbc84bc34fac51bc0b85
Juhana Lankinen <juhana.lankinen@csc.fi>
2024-04-02 16:07:00 +0300

    Add sycl pixel sample

---
+++ samples/sycl_samples/pixels_sample.cpp
@@ -0,0 +1,47 @@
+/*
+    aosoa
+    Copyright (C) 2024  Juhana Lankinen
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "common.h"
+#include "sycl_memory_operations.h"
+
+using PixelSoa = Soa<SyclDeviceMemoryOperations>;
+using Pixels = Acc<SyclDeviceMemoryOperations>;
+
+int deviceSelector() {}
+
+int main(int , char **) {
+    sycl::device d(sycl::gpu_selector_v);
+    sycl::property_list q_prop{sycl::property::queue::in_order()};
+    sycl::queue queue{d, q_prop};
+
+    aosoa::SyclMemoryOperations memory_ops{
+        SyclAllocator{queue}, SyclDeallocator{queue}, SyclMemcpy{queue},
+        SyclMemset{queue}};
+
+    Pixels *d_pixels = sycl::malloc_device<Pixels>(1, queue);
+    PixelSoa pixel_soa(memory_ops, num_pixels, d_pixels);
+
+    [[maybe_unused]] auto event = queue.submit([&](sycl::handler &h) {
+        h.parallel_for(num_pixels,
+                       [=](auto idx) { computeColor(idx, d_pixels); });
+    });
+    writePixelsToFile(pixel_soa, "pixels_sycl.png");
+    sycl::free(d_pixels, queue);
+
+    return 0;
+}
