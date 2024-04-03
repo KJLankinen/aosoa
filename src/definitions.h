7e3b87c7b6c5adcc31795842cabe0b0eb903463d
Juhana Lankinen <juhana.lankinen@csc.fi>
2024-04-03 15:52:27 +0300

    Add rocm check for host device def

--- src/definitions.h
+++ src/definitions.h
@@ -25,7 +25,9 @@
 #pragma once
 
 // clang-format off
-#if defined(__NVCC__) || (defined(__clang__) && defined(__CUDA__))
+#if defined(__NVCC__) || \
+    (defined(__clang__) && defined(__CUDA__)) || \
+    defined(__HIPCC__)
     #define HOST __host__
     #define DEVICE __device__
 #else
